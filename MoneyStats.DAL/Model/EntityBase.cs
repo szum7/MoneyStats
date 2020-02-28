b3f2eeef28c05899b76982656e3e6a3fa32cbd4e
Aron Szocs <arsz01@betssongroup.com>
2020-02-28 16:51:40 +0100

    misc

---
+++ MoneyStats.DAL/Model/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoneyStats.DAL.Model
+{
+    public class EntityBase
+    {
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int? CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int State { get; set; }
+    }
+}
--- MoneyStats.DAL/Model/Transaction.cs
+++ MoneyStats.DAL/Model/Transaction.cs
@@ -5,8 +5,27 @@ using System.Text;
 
 namespace MoneyStats.DAL.Model
 {
-    [Table("Transaction")]
-    public class Transaction
+    [Table("TransactionTagConn")]
+    public class TransactionTagConn
     {
+
+    }
+
+    [Table("Transaction")]
+    public class Transaction : EntityBase
+    {
+        public int Id { get; set; }
+        public DateTime AccountingDate { get; set; }
+        public string TransactionId { get; set; } // TODO átnevezni, ne legyen class+id neve
+        public string Type { get; set; }
+        public string Account { get; set; }
+        public string AccountName { get; set; }
+        public string PartnerAccount { get; set; }
+        public string PartnerName { get; set; }
+        public decimal? Sum { get; set; }
+        public string Currency { get; set; }
+        public string Message { get; set; }
+
+        public virtual ICollection<TransactionTagConn> TransactionTagConn { get; set; }
     }
 }
