b84dd30bab1ff44d4208a0e36b73fbfbe11f8ed5
Aron Szocs <arsz01@betssongroup.com>
2020-02-28 16:04:01 +0100

    add starting projects

---
+++ MoneyStats.DAL/Model/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MoneyStats.DAL.Model
+{
+    [Table("Transaction")]
+    public class Transaction
+    {
+    }
+}
---
+++ MoneyStats.DAL/MoneyStatsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyStats.DAL.Model;
+
+namespace MoneyStats.DAL
+{
+    public class MoneyStatsContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"");
+            }
+        }
+    }
+}
