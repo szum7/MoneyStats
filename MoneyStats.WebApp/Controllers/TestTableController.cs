0be2fb778f9a861f1ff8d5c9a69ecb98d7bd773d
Aron Szocs <arsz01@betssongroup.com>
2020-03-03 14:15:43 +0100

    Add WebApp

---
+++ MoneyStats.WebApp/Controllers/TestTableController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyStats.BL;
+using MoneyStats.BL.Repositories;
+
+namespace MoneyStats.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestTableController : ControllerBase
+    {
+        [HttpGet("testcall")]
+        public ActionResult TestCall()
+        {
+            return Ok(new { message = "Test call got it's response. I am that response." });
+        }
+
+        [HttpGet("get")]
+        public ActionResult Get()
+        {
+            var repo = new TransactionRepository();
+            return Ok(repo.Get());
+        }
+    }
+}
