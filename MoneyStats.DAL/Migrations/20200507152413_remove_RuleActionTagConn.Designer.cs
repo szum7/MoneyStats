e8b8c3a9eb79ab76f0758a94a97dec332811a610
Szőcs Áron <szocs.aa@gmail.com>
2020-05-07 17:25:23 +0200

    add remove_ActionTagConn migration

--- MoneyStats.DAL/Migrations/20200507152413_remove_RuleActionTagConn.Designer.cs
+++ MoneyStats.DAL/Migrations/20200507152413_remove_RuleActionTagConn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoneyStats.DAL;
 
 namespace MoneyStats.DAL.Migrations
 {
     [DbContext(typeof(MoneyStatsContext))]
-    partial class MoneyStatsContextModelSnapshot : ModelSnapshot
+    [Migration("20200507152413_remove_RuleActionTagConn")]
+    partial class remove_RuleActionTagConn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
