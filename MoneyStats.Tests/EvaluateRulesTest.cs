dfd642a4375b3ba4d304cbf95c68bef863fb8ea6
Szőcs Áron <szocs.aa@gmail.com>
2020-05-27 23:52:49 +0200

    EvalRule test setup

---
+++ MoneyStats.Tests/EvaluateRulesTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MoneyStats.BL.Interfaces;
+using MoneyStats.BL.Repositories;
+using MoneyStats.DAL.Models;
+using Newtonsoft.Json;
+
+namespace MoneyStats.Tests
+{
+    public class EvaluateRulesTestInput
+    {
+        public List<BankRow> BankRows { get; set; }
+        public List<RuleGroup> RuleGroups { get; set; }
+    }
+
+    public class EvaluateRulesTestOutput
+    {
+        public List<Transaction> Transactions { get; set; }
+        public List<TransactionCreatedWithRule> TransactionCreatedWithRules { get; set; }
+        public List<TransactionTagConn> TransactionTagConns { get; set; }
+    }
+
+    [TestClass]
+    public class EvaluateRulesTest
+    {
+        [TestMethod]
+        public void TestTransactionGroup()
+        {
+            // Arrange
+            var input = new EvaluateRulesTestInput()
+            {
+                BankRows = new List<BankRow>()
+                {
+
+                },
+                RuleGroups = new List<RuleGroup>()
+                {
+
+                }
+            };
+
+            var output = new EvaluateRulesTestOutput()
+            {
+                Transactions = new List<Transaction>()
+                {
+
+                },
+                TransactionCreatedWithRules = new List<TransactionCreatedWithRule>()
+                {
+
+                },
+                TransactionTagConns = new List<TransactionTagConn>()
+                {
+
+                }
+            };
+
+            // Act & Assert
+            this.ActAndAssert(input, output);
+        }
+
+        public void ActAndAssert(EvaluateRulesTestInput input, EvaluateRulesTestOutput output)
+        {
+            // Act
+            var result = new RuleRepository().CreateTransactionUsingRulesFlattened(input.RuleGroups, input.BankRows);
+
+            // Assert
+            Assert.AreEqual(result.Transactions.Count, output.Transactions.Count);
+            Assert.AreEqual(result.TransactionTagConns.Count, output.TransactionTagConns.Count);
+            Assert.AreEqual(result.TransactionCreatedWithRules.Count, output.TransactionCreatedWithRules.Count);
+
+            for (int i = 0; i < result.Transactions.Count; i++)
+            {
+                var a = JsonConvert.SerializeObject(result.Transactions[i]);
+                var b = JsonConvert.SerializeObject(output.Transactions[i]);
+                Assert.AreEqual(a, b);
+            }
+            for (int i = 0; i < result.TransactionTagConns.Count; i++)
+            {
+                var a = JsonConvert.SerializeObject(result.TransactionTagConns[i]);
+                var b = JsonConvert.SerializeObject(output.TransactionTagConns[i]);
+                Assert.AreEqual(a, b);
+            }
+            for (int i = 0; i < result.TransactionCreatedWithRules.Count; i++)
+            {
+                var a = JsonConvert.SerializeObject(result.TransactionCreatedWithRules[i]);
+                var b = JsonConvert.SerializeObject(output.TransactionCreatedWithRules[i]);
+                Assert.AreEqual(a, b);
+            }
+        }
+    }
+}
