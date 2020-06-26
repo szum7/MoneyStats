c196bc889af73d019da0b4d0719027f54f6f7498
Szőcs Áron <szum7@hotmail.com>
2020-06-26 11:44:18 +0200

    bat files

---
+++ OpenDevEnvironment.bat
@@ -0,0 +1,11 @@
+rem Open Visual Studio Code
+start code "D:\CodeProjects\MoneyStats\MoneyStats.WebApp\ClientApp"
+
+rem Open Visual Studio
+start "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe" "D:\CodeProjects\MoneyStats\MoneyStats.sln"
+
+rem Navigate to the ClientApp for ng serve
+D:
+cd "D:\CodeProjects\MoneyStats\MoneyStats.WebApp\ClientApp"
+
+cmd /k
